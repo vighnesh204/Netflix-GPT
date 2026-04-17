4c4eda9cb179695db8f9a84f9da1ab1348915984
vighnesh204 <borhadevighnesh@gmail.com>
2026-04-17 17:41:45 +0530

    implement form validation with useRef and create validate.js

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,24 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Header from "./Header";
 import { BACKGROUND_IMAGE } from "../utils/constants";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef(null)
+  const password = useRef(null)
+
+  const handleButtonClick = () => {
+    // Validate the form data
+    console.log(email.current.value)
+    console.log(password.current.value)
+
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
+    console.log(message)
+  }
 
   const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm);
@@ -17,7 +32,7 @@ const Login = () => {
           alt=""
         />
       </div>
-      <form className="w-4/12 absolute p-12 bg-black/80 text-white my-24 mx-auto right-0 left-0 rounded-md shadow-2xl">
+      <form onSubmit={(e)=> e.preventDefault()} className="w-4/12 absolute p-12 bg-black/80 text-white my-24 mx-auto right-0 left-0 rounded-md shadow-2xl">
         <h1 className="text-3xl font-bold mb-6">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
@@ -31,20 +46,25 @@ const Login = () => {
         )}
 
         <input
+        ref={email}
           type="email"
           placeholder="Email or phone number"
           className="w-full p-3 mb-4 bg-zinc-800 rounded text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-red-600"
         />
 
         <input
+        ref={password}
           type="password"
           placeholder="Password"
           className="w-full p-3 mb-6 bg-zinc-800 rounded text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-red-600"
         />
+         
+         <p className="text-red-500 font-semibold mb-2">{errorMessage}</p>
 
         <button
           type="submit"
           className="w-full bg-red-600 hover:bg-red-700 transition duration-300 py-3 rounded font-semibold"
+          onClick={handleButtonClick}
         >
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
---
+++ src/utils/validate.js
@@ -0,0 +1,11 @@
+export const checkValidData = (email, password) => {
+  const isEmailValid = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/.test(
+    email,
+  );
+  const isPasswordValid =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/.test(password);
+
+  if (!isEmailValid) return "Email is not valid";
+  if (!isPasswordValid) return "Password is not valid";
+  return null;
+};
