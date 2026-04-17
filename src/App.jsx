468433c6c01ef9f52f41a540e06dea2acead4fa6
vighnesh204 <borhadevighnesh@gmail.com>
2026-04-17 10:59:43 +0530

    create basic components and configure basic routing using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Body from './components/Body'
 
 const App = () => {
   return (
-    <div className='text-2xl'>App</div>
+    <div>
+      <Body />
+    </div>
   )
 }
 
